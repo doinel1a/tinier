24f4236b37fff86373393d5f735ad735b4f25b95
doinel1a <doinel1atanasiu@gmail.com>
2022-07-17 14:00:30 +0200

    Basic api endpoint w/ redirect implemented

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { prisma } from '../../db/client';
+
+export default async function (
+    request: NextApiRequest,
+    response: NextApiResponse
+) {
+    const slug = request.query['slug'];
+
+    if (!slug || typeof slug !== 'string') {
+        response.statusCode = 404;
+        response.send(
+            JSON.stringify({
+                message: 'Please provide a slug!',
+            })
+        );
+
+        return;
+    }
+
+    const data = await prisma.link.findFirst({
+        where: {
+            slug: {
+                equals: slug,
+            },
+        },
+    });
+
+    if (!data) {
+        response.statusCode = 404;
+        response.send(
+            JSON.stringify({
+                message: 'Slug not found!',
+            })
+        );
+
+        return;
+    }
+
+    return response.redirect(data.url);
+
+    // return response.json(data);
+}
