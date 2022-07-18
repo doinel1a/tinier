dc59286228c5cb12ca4af9e629f07375873aad35
doinel1a <doinel1atanasiu@gmail.com>
2022-07-18 08:57:52 +0200

    TRPC setted up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import { withTRPC } from '@trpc/next';
+import { AppRouter } from './api/trpc/[trpc]';
 
 import { ContextProvider } from '../context/ContextProvider';
 
@@ -17,4 +19,19 @@ const App = ({ Component, pageProps }: AppProps) => {
     );
 };
 
-export default App;
+function getBaseUrl() {
+    if (process.browser) return '';
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+    return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+export default withTRPC<AppRouter>({
+    config() {
+        const url = `${getBaseUrl()}/api/trpc`;
+
+        return {
+            url,
+        };
+    },
+})(App);
