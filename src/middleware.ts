56ea06ea1d95865fd9b770715c853ffff40d7ae0
doinel1a <doinel1atanasiu@gmail.com>
2022-07-18 08:56:10 +0200

    Middleware implemented

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+    if (
+        request.nextUrl.pathname.startsWith('/api/') ||
+        request.nextUrl.pathname === '/'
+    ) {
+        console.warn('RETURNING EARLY!');
+        return;
+    }
+
+    const slugParameter = request.nextUrl.pathname.split('/').pop();
+
+    const slugFetched = await fetch(
+        `${request.nextUrl.origin}/api/get-url/${slugParameter}`
+    );
+
+    if (slugFetched.status === 404) return;
+
+    const data = await slugFetched.json();
+
+    if (data?.url) return NextResponse.redirect(data.url);
+}
